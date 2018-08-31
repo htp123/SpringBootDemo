da099da5802d22a912ea135655e2e9ae8a1eabe9
hutp <13218063373@163.com>
2018-08-31 16:51:22 +0800

    默认首页

---
+++ src/main/java/com/example/configure/DefaultView.java
@@ -0,0 +1,20 @@
+package com.example.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Administrator on 2018/8/31.
+ */
+
+//@Configuration
+public class DefaultView extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    }
+}
